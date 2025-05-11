3c47ae4b680f46f28b196fb2c5a174090dddbc09
Jay Rainey <jawrainey@gmail.com>
2025-05-11 18:58:21 +0100

    bump Java version and JVM target

--- app/src/main/java/com/example/hfta/MainActivity.kt
+++ app/src/main/java/com/example/hfta/MainActivity.kt
@@ -9,8 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
